3675c55142e73e7b7fc11acd36df6ef2683744ed
timmonfette1 <monfette.timothy@pm.me>
2026-08-14 22:10:52 -0400

    Adding utility functions for 'validate'

--- protago.go
+++ protago.go
@@ -2,6 +2,7 @@ package protago
 
 import (
 	"github.com/timmonfette1/protago/internal/genproto/protago/bson/v1"
+	"github.com/timmonfette1/protago/internal/genproto/protago/validate/v1"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/descriptorpb"
@@ -13,6 +14,10 @@ type BsonOptions struct {
 	Minsize   bool
 	Truncate  bool
 	Inline    bool
+}
+
+type ValidateOptions struct {
+	Rule string
 }
 
 func openBsonFieldOptions(bOpt *bson.BsonFieldOptions) *BsonOptions {
@@ -27,6 +32,14 @@ func openBsonFieldOptions(bOpt *bson.BsonFieldOptions) *BsonOptions {
 		Truncate:  bOpt.GetTruncate(),
 		Inline:    bOpt.GetInline(),
 	}
+}
+
+func openValidateOptions(vOpt *validate.ValidateFieldOptions) *ValidateOptions {
+	if vOpt == nil {
+		return nil
+	}
+
+	return &ValidateOptions{Rule: vOpt.GetRule()}
 }
 
 func GetAllBsonOptions(msg proto.Message) map[string]*BsonOptions {
@@ -45,4 +58,22 @@ func GetBsonOptions(fd protoreflect.FieldDescriptor) *BsonOptions {
 	opts := fd.Options().(*descriptorpb.FieldOptions)
 	bOpts := proto.GetExtension(opts, bson.E_Options).(*bson.BsonFieldOptions)
 	return openBsonFieldOptions(bOpts)
+}
+
+func GetAllValidateOptions(msg proto.Message) map[string]*ValidateOptions {
+	vOpts := make(map[string]*ValidateOptions)
+	m := msg.ProtoReflect()
+	m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		vOpt := GetValidateOptions(fd)
+		vOpts[string(fd.Name())] = vOpt
+		return true
+	})
+
+	return vOpts
+}
+
+func GetValidateOptions(fd protoreflect.FieldDescriptor) *ValidateOptions {
+	opts := fd.Options().(*descriptorpb.FieldOptions)
+	vOpts := proto.GetExtension(opts, validate.E_Options).(*validate.ValidateFieldOptions)
+	return openValidateOptions(vOpts)
 }
--- protago_test.go
+++ protago_test.go
@@ -1,7 +1,6 @@
 package protago
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,7 +9,6 @@ import (
 )
 
 func TestGetAllBsonOptions(t *testing.T) {
-	fmt.Println("HELLO")
 	ks := "cool sink"
 	a := int32(30)
 	s := true
@@ -100,4 +98,77 @@ func TestGetBsonOptions(t *testing.T) {
 	assert.True(t, bsonOpts.Minsize)
 	assert.True(t, bsonOpts.Truncate)
 	assert.True(t, bsonOpts.Inline)
+}
+
+func TestGetAllValidateOptions(t *testing.T) {
+	e := "mock@unittest.com"
+	s := true
+	input := &testdata.TestValidate{
+		Id:    "id",
+		Email: &e,
+		Skip:  &s,
+	}
+
+	validateOpts := GetAllValidateOptions(input)
+	assert.Len(t, validateOpts, 3)
+
+	type args struct {
+		name  string
+		isNil bool
+		opts  *ValidateOptions
+	}
+	expected := []args{
+		{
+			name: "id",
+			opts: &ValidateOptions{
+				Rule: "validate:\"required,omitempty,max=255\"",
+			},
+		},
+		{
+			name: "email",
+			opts: &ValidateOptions{
+				Rule: "validate:\"required,hostname_rfc1123\"",
+			},
+		},
+		{
+			name:  "skip",
+			isNil: true,
+			opts:  nil,
+		},
+	}
+
+	for _, arg := range expected {
+		t.Run(arg.name, func(t *testing.T) {
+			v, ok := validateOpts[arg.name]
+			assert.True(t, ok)
+
+			if arg.isNil {
+				assert.Nil(t, v)
+			} else {
+				assert.NotNil(t, v)
+				assert.Equal(t, *arg.opts, *v)
+			}
+		})
+	}
+}
+
+func TestGetValidateOptions(t *testing.T) {
+	input := &testdata.TestValidate{
+		Id: "id",
+	}
+
+	var idField protoreflect.FieldDescriptor
+	m := input.ProtoReflect()
+	m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		if fd.Name() == "id" {
+			idField = fd
+			return false
+		}
+		return true
+	})
+	assert.NotNil(t, idField)
+
+	validateOpts := GetValidateOptions(idField)
+	assert.NotNil(t, validateOpts)
+	assert.Equal(t, "validate:\"required,omitempty,max=255\"", validateOpts.Rule)
 }
